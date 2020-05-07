8f19c4d8c4baebea07b45a8ecbb716d056c98ef1
Dylan Harty <me@dylanharty.com>
2020-05-06 21:15:13 -0600

    minor updates

--- generator/index.js
+++ generator/index.js
@@ -2,15 +2,12 @@ module.exports = (api, options) => {
   api.render('./template', { ...options, START_YEAR: new Date().getFullYear(), BASE_URL: '<%= BASE_URL %>', htmlWebpackPlugin:'<%= htmlWebpackPlugin.options.title %>' });
   api.extendPackage({
     dependencies: {
-      'everflow': '^4.0.0-alpha.3'
+      'everflow': '^4.0.0-alpha.4'
     },
     devDependencies: {
+      "everflow-webpack-plugin": "^0.1.0-alpha.1",
       "@everflow-cli/tools": "^0.1.2",
       "terser-webpack-plugin": "^3.0.0"
-    },
-    scripts: {
-      "preserve": "node ./node_modules/@everflow-cli/tools/magic-routes.js",
-      "prebuild": "node ./node_modules/@everflow-cli/tools/magic-routes.js"
     }
   });
   if (api.hasPlugin('pwa'))
--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-module.exports = (api, options) => {};
+const EverflowWebpackPlugin = require('everflow-webpack-plugin');
+
+module.exports = (api, options) => {
+
+    // Add everflow plugin to webpack config for VueJS
+    api.chainWebpack((config) => {
+        config
+          .plugin('everflowWebpackPlugin')
+              .use(EverflowWebpackPlugin)
+                  .end();
+    });
+
+};
